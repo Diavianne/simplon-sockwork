482db8a2c3732559864c970c9c1f8b2230f91419
Émilie Barré <126564806+Diavianne@users.noreply.github.com>
2024-11-19 22:56:59 +0100

    Delete sockwork-presentation directory

--- sockwork-presentation/src/views/AccountCreateView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import AccountCreate from "@/components/AccountCreate.vue";
-</script>
-
-<template>
-  <div>
-    <AccountCreate />
-  </div>
-</template>
--- sockwork-presentation/src/views/NotFoundView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <h1>not found</h1>
-</template>
-<style scoped>
-h1 {
-  color: red;
-}
-</style>
