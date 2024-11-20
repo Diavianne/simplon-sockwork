3bc4e6b1dea8f6fd3e82257652fb45e64f5a0bb2
Émilie Barré <diavianne@gmail.com>
2024-11-20 23:48:42 +0100

    Update Script.sql

--- socwork-db/Scripts/Script.sql
+++ socwork-db/Scripts/Script.sql
@@ -1,14 +1,22 @@
 DROP TABLE IF EXISTS t_accounts;
 
 
+
+
 CREATE TABLE t_accounts (
-    account_id INT GENERATED ALWAYS AS IDENTITY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(255),
-    password VARCHAR (16),
-    CONSTRAINT t_accounts_pkey PRIMARY KEY (account_id)
+    password VARCHAR (72),
+    CONSTRAINT t_account_pkey PRIMARY KEY (id),
+    CONSTRAINT t_account_ukey unique (username)
 );
 
 
+
+
+
 SELECT * FROM t_accounts;
+
+
 
 --TRUNCATE TABLE t_accounts;
